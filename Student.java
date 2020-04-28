59be3785f5afed9c36a9e9b0adf61b83b9dd1a74
L202 <1011543252@qq.com>
2020-04-28 11:44:26 +0800

    添加 注释 Student.java

--- Student.java
+++ Student.java
@@ -1,12 +1,19 @@
 public class Student{
 
-	private Integer sid;
-	private String name;
+	private Integer sid;   //编号
+	private String name;   //名字
 	
+	//get和set方法
 	public void setSid(Integer sid){
 	    this.sid = sid;
 	}
 	public Integer getSid(){
             return sid;
+	}
+	public void setName(String name){
+	    this.name = name;
+	}
+	public String getName(){
+            return name;
 	}
 }
