da0fae770bade0c5592f3429f445d29a85acba9f
L2 <L2@imit.com>
2020-04-28 09:20:57 +0800

    提交 Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student{
+
+	private Integer sid;
+	private String name;
+	
+	public void setSid(Integer sid){
+	    this.sid = sid;
+	}
+	public Integer getSid(){
+            return sid;
+	}
+}
